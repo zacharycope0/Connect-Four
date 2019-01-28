07b812c695a46629e4b99dad7fdd2b3b4212c370
zacharycope0 <zacharycope0@gmail.com>
2019-01-28 17:09:15 -0500

    Changes
    Updated the test file to validate inputs and stop game after the board is filled.

--- Connect_Four/Test.py
+++ Connect_Four/Test.py
@@ -7,19 +7,34 @@ bc = BoardChecker(b)
 
 bc.board.toString()
 
-count = 0
+count_moves = 0
 
-while True:
+while count_moves<56:
+
+    while True:
+        try:
+            input_move = int(input('Player 1:'))
+            while bc.board.check_move(input_move) != True:
+                input_move = int(input('No space in column. Try again:'))
+            break
+        except:
+            print("Value out of range try again")
 
     token = 'X'
-    bc.board.make_move(int(input('Player 1:')),token)
+    bc.board.make_move(input_move,token)
+    count_moves += 1
     bc.board.toString()
     if bc.has_winner() == True:
         print('Player 1 wins!')
         break
+
+    input_move = int(input('Player 2:'))
+    while bc.board.check_move(input_move) != True:
+	    input_move = int(input('No space in column. Try again:'))
             
     token = 'O'
-    bc.board.make_move(int(input('Player 2:')),token)
+    bc.board.make_move(input_move,token)
+    count_moves += 1
     bc.board.toString()
     if bc.has_winner() == True:
         print('Player 2 wins!')
